b0b46a33217974aca11e1af6573cd57492c2b252
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-02-24 11:35:46 -0300

    alter: paginations item

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Stack } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
+import { PaginationItem } from "./PaginationItem"
 
 export function Pagination() {
     return (
@@ -13,30 +14,8 @@ export function Pagination() {
                 <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
             </Box>
             <Stack direction="row" spacing="2">
-                <Button
-                    size="sm"
-                    fontSize="xs"
-                    w="4"
-                    colorScheme="pink"
-                    disabled
-                    _disabled={{
-                        bgColor: "pink.500",
-                        cursor: "default",
-                    }}
-                >
-                    1
-                </Button>
-                <Button
-                    size="sm"
-                    fontSize="xs"
-                    w="4"
-                    bgColor="gray.700"
-                    _hover={{
-                        bgColor: "gray.500",
-                    }}
-                >
-                    2
-                </Button>
+                <PaginationItem number={1} isCurrent />
+                <PaginationItem number={2} />
             </Stack>
         </Stack>
     )
