bf891f9540b0635ec624e72e1167d6c67e575739
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-02-24 13:06:14 -0300

    alter: differentiating active page in sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -4,8 +4,9 @@ import {
     LinkProps as ChakraLinkProps,
     Text,
 } from "@chakra-ui/react"
+import Link from "next/link"
 import { ElementType } from "react"
-import Link from 'next/link'
+import { ActiveLink } from "../ActiveLink"
 
 interface NavLinkProps extends ChakraLinkProps {
     icon: ElementType
@@ -14,14 +15,26 @@ interface NavLinkProps extends ChakraLinkProps {
 }
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
+    if(href == '/') {
+        return (
+            <Link href={href} passHref>
+                <ChakraLink display="flex" color="gray.50" {...rest}>
+                    <Icon as={icon} fontSize="20" />
+                    <Text ml="4" fontWeight="medium">
+                        {children}
+                    </Text>
+                </ChakraLink>
+            </Link>
+        )
+    }
     return (
-        <Link href={href} passHref>
+        <ActiveLink href={href} passHref>
             <ChakraLink display="flex" color="pink.400" {...rest}>
                 <Icon as={icon} fontSize="20" />
                 <Text ml="4" fontWeight="medium">
                     {children}
                 </Text>
             </ChakraLink>
-        </Link>
+        </ActiveLink>
     )
 }
