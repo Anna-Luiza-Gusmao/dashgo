b8189ee59bb68024a0e1ae0ac772b62bd973ba9c
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-02-24 07:36:39 -0300

    add: sidebar

---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,24 @@
+import {
+    Icon,
+    Link as ChakraLink,
+    LinkProps as ChakraLinkProps,
+    Text,
+} from "@chakra-ui/react"
+import { ElementType } from "react"
+
+interface NavLinkProps extends ChakraLinkProps {
+    icon: ElementType
+    children: string
+    href: string
+}
+
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
+    return (
+        <ChakraLink display="flex" color="pink.400" {...rest}>
+            <Icon as={icon} fontSize="20" />
+            <Text ml="4" fontWeight="medium">
+                {children}
+            </Text>
+        </ChakraLink>
+    )
+}
